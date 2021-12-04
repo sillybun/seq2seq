4284e078771a5842f40f7e0705b0f7a30e2af5db
zhangyiteng <zhangyiteng@zhangyitengdeiMac.local>
2021-12-04 18:49:37 +0800

    update

---
+++ main.py
@@ -0,0 +1,129 @@
+from trainer import Trainer
+from zytlib import vector, path
+from zytlib import Logger
+from zytlib.table import table
+from zytlib.wrapper import repeat_trigger
+from tqdm import tqdm
+import argparse
+import sys
+from torchfunction.inspect import get_shape
+import torch
+
+def main(parser, **kwargs):
+
+    hyper = table({
+            "max_epochs": 100,
+            "timer_disable": True,
+            "encoder_max_rank": 20,
+            })
+    hyper.update(kwargs)
+
+    t = Trainer(**hyper)
+
+    if hyper["load_model_path"] != "":
+        t.load_state_dict(hyper["load_model_path"])
+
+    logger = Logger(True, True)
+    logger.info("running command: python " + " ".join(sys.argv))
+    logger.log_parser(parser)
+    logger.info(t.hyper)
+    logger.info(t.named_parameters().map(lambda name, x: (name, x.shape, x.requires_grad)))
+
+    if hyper["model_name"]:
+        saved_path = path("model") / path(hyper["model_name"]).with_ext("pt")
+    else:
+        saved_path = path("model") / logger.f_name.with_ext("pt")
+
+    checkpoint_dir = (path("model/checkpoint/") / logger.f_name.with_ext("")).mkdir()
+
+    @repeat_trigger(lambda ret: t.save(checkpoint_dir / logger.f_name.with_ext("pt").name_add("_{}".format(ret.ret))), n=hyper["save_model_every_epoch_num"], start=hyper["save_model_every_epoch_num"])
+    def loop(epoch):
+        train_loss, train_acc = vector(), vector()
+
+        for index, batch in tqdm(enumerate(t.train_dataloader), total=len(t.train_dataloader)):
+            loss, acc = t.train_step(batch, epoch, index)
+            train_loss.append(loss)
+            train_acc.append(acc)
+
+        if isinstance(t.test_dataloader, dict):
+            test_loss, test_acc = table(), table()
+
+            for name, dl in t.test_dataloader.items():
+                tl, ta = vector(), vector()
+
+                for index, batch in tqdm(enumerate(dl), total=len(dl)):
+                    loss, acc = t.test_step(batch, epoch, index)
+                    tl.append(loss)
+                    ta.append(acc)
+
+                test_loss[name] = tl
+                test_acc[name] = ta
+
+            logger.info(f"[{epoch}]/[{hyper['max_epochs']}], train_loss: {train_loss.mean()}, train_acc: {train_acc.mean()}, test_loss: {test_loss.map(value=lambda x: x.mean())}, test_acc: {test_acc.map(value=lambda x: x.mean())}")
+
+            logger.variable("train_loss", train_loss.mean())
+            logger.variable("train_acc", train_acc.mean())
+
+            for name, tl in test_loss.items():
+                logger.variable("test_loss[{}]".format(name), tl.mean())
+            for name, tc in test_acc.items():
+                logger.variable("test_acc[{}]".format(name), tc.mean())
+
+        else:
+            test_loss, test_acc = vector(), vector()
+
+            for index, batch in tqdm(enumerate(t.test_dataloader), total=len(t.test_dataloader)):
+                loss, acc = t.test_step(batch, epoch, index)
+                test_loss.append(loss)
+                test_acc.append(acc)
+
+            logger.info(f"[{epoch}]/[{hyper['max_epochs']}], train_loss: {train_loss.mean()}, train_acc: {train_acc.mean()}, test_loss: {test_loss.mean()}, test_acc: {test_acc.mean()}")
+
+            logger.variable("train_loss", train_loss.mean())
+            logger.variable("train_acc", train_acc.mean())
+            logger.variable("test_loss", test_loss.mean())
+            logger.variable("test_acc", test_acc.mean())
+
+
+        for key, value in t.inspect().items():
+            logger.variable(key, value)
+        return epoch
+
+    try:
+        for epoch in range(hyper["max_epochs"]):
+            loop(epoch)
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+    logger.plot_variable_dict(logger.variable_dict, saved_path=path("Log") / path("plot") / path("trainingstep_" + logger.f_name).with_ext("pdf"), hline=["bottom"])
+    t.save(saved_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--max_epochs", default=500, type=int)
+    parser.add_argument("--embedding_trainable", dest="is_embedding_fixed", action="store_false")
+    parser.add_argument("--delta_t", default=20, type=int)
+    parser.add_argument("--tau", default=100, type=float)
+    parser.add_argument("--embedding_size", default=1024, type=int)
+    parser.add_argument("--decoder_dim", default=512, type=int)
+    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument("--noise_sigma", default=0.05, type=float)
+    parser.add_argument("--learning_rate", default=2e-5, type=float)
+    parser.add_argument("--datapath", default="dataset/dataset_rank_3_num_items_6_overlap_1_repeat_100.db", type=str)
+    parser.add_argument("--embedding", default="dataset/embedding_inputdim_6_embeddingdim_1024_round.db", type=str)
+    parser.add_argument("--non_linear_decoder", dest="linear_decoder", action="store_false")
+    parser.add_argument("--l2_reg", default=1e-5, type=float)
+    parser.add_argument("--encoder_bias", action="store_true")
+    parser.add_argument("--decoder_bias", action="store_true")
+    parser.add_argument("--encoder_max_rank", default=-1, type=int)
+    parser.add_argument("--decoder_max_rank", default=-1, type=int)
+    parser.add_argument("--model_name", default="", type=str)
+    parser.add_argument("--load_model_path", default="", type=str)
+    parser.add_argument("--freeze_parameter", nargs="+")
+    parser.add_argument("--save_model_every_epoch_num", default=100, type=int)
+    parser.add_argument("--timer_enable", dest="timer_disable", action="store_false")
+    hyper, unknown = parser.parse_known_args()
+    print("unknown", unknown)
+    hyper = table(hyper)
+    main(parser, **hyper)
---
+++ trainer.py
@@ -0,0 +1,168 @@
+from pyctlib import vector
+import torch
+import torch.nn as nn
+from model import encoder, decoder
+from dataset import SimulatedDataset, SimulateEmbedding
+from torchfunction.device import todevice
+from zytlib import vector
+from zytlib.table import table
+from zytlib import path
+from torchfunction.device import get_device
+from typing import List, Tuple
+from zytlib.wrapper import FunctionTimer
+
+class Trainer:
+
+    def __init__(self, **kwargs):
+        self.hyper = table({"datapath": "dataset/dataset_rank_3_num_items_6_overlap_1_repeat_100.db",
+                "delta_t": 20,
+                "tau": 100,
+                "embedding_size": 1024,
+                "decoder_dim": 512,
+                "noise_sigma": 0.05,
+                "input_dim": 6,
+                "batch_size": 32,
+                "linear_decoder": True,
+                "device": "cuda",
+                "embedding": "dataset/embedding_inputdim_6_embeddingdim_1024_round.db",
+                "learning_rate": 2e-5,
+                "is_embedding_fixed": True,
+                "l2_reg": 1e-5,
+                "encoder_bias": False,
+                "decoder_bias": False,
+                "encoder_to_decoder_equal_space": False,
+                "encoder_max_rank": -1,
+                "decoder_max_rank": -1,
+                "freeze_parameter": [],
+                "timer_disable": True,
+                })
+        self.hyper.update_exist(kwargs)
+        if self.hyper.key_not_here(kwargs):
+            print("unknown key:", self.hyper.key_not_here(kwargs))
+
+        self.device = get_device(self.hyper["device"])
+        self.timer = FunctionTimer(disable=self.hyper.timer_disable)
+        if isinstance(self.hyper["embedding"], str):
+            self.hyper["embedding"] = SimulateEmbedding.load(self.hyper["embedding"]).embedding
+        self.encoder = encoder(self.hyper["tau"],
+                 self.hyper["delta_t"],
+                 self.hyper["embedding_size"],
+                 self.hyper["noise_sigma"],
+                 self.hyper["input_dim"],
+                 embedding = self.hyper["embedding"],
+                 is_embedding_fixed = self.hyper["is_embedding_fixed"],
+                 encoder_bias = self.hyper["encoder_bias"],
+                 encoder_max_rank = self.hyper["encoder_max_rank"],
+                 timer = self.timer.timer).to(self.device)
+        self.encoder.embedding = self.encoder.embedding.to(self.device)
+        self.decoder = decoder(self.hyper["decoder_dim"],
+                 self.hyper["embedding_size"],
+                 self.hyper["input_dim"],
+                 self.hyper["input_dim"],
+                 self.encoder.embedding,
+                 linear_decoder = self.hyper["linear_decoder"],
+                 decoder_bias = self.hyper["decoder_bias"],
+                 encoder_to_decoder_equal_space = self.hyper["encoder_to_decoder_equal_space"],
+                 decoder_max_rank = self.hyper["decoder_max_rank"],
+                 timer = self.timer.timer).to(self.device)
+
+        for name, param in self.named_parameters():
+            if vector(self.hyper["freeze_parameter"]).any(lambda x: name.startswith(x)):
+                param.requires_grad = False
+
+        self.dataset = SimulatedDataset(self.hyper["datapath"], self.hyper["input_dim"], self.hyper["batch_size"])
+
+    def train_step(self, batch, epoch=-1, index=-1):
+        self.encoder.train()
+        self.decoder.train()
+        self.optimizer.zero_grad()
+        input_encoder, length, ground_truth_tensor, ground_truth_length = todevice(batch, device=self.hyper["device"])
+        hidden_state, final_state = self.encoder(input_encoder, length)
+        decoded_seq, hidden_state_decoder = self.decoder(final_state, ground_truth_tensor, ground_truth_length, teaching_forcing_ratio=0.5)
+
+        loss = self.decoder.loss(ground_truth_tensor, ground_truth_length, decoded_seq)
+        l2_reg = 0
+        for reg_param in (self.encoder.regulization_parameters + self.decoder.regulization_parameters):
+            l2_reg += torch.sum(torch.square(reg_param))
+        loss = loss + self.hyper["l2_reg"] * l2_reg
+        accuracy = self.decoder.accuracy(ground_truth_tensor, ground_truth_length, decoded_seq)
+        loss.backward()
+        self.optimizer.step()
+        return loss.item(), accuracy.item()
+
+    def test_step(self, batch, epoch, index):
+        self.encoder.eval()
+        self.decoder.eval()
+        input_encoder, length, ground_truth_tensor, ground_truth_length, in_train = todevice(batch, device=self.hyper["device"])
+        with torch.no_grad():
+            hidden_state, final_state = self.encoder(input_encoder, length)
+            decoded_seq, hidden_state_seq = self.decoder(final_state, torch.zeros_like(ground_truth_tensor).fill_(-1), ground_truth_length, teaching_forcing_ratio=0.0)
+
+            loss = self.decoder.loss(ground_truth_tensor, ground_truth_length, decoded_seq)
+            accuracy = self.decoder.accuracy(ground_truth_tensor, ground_truth_length, decoded_seq)
+        return loss.item(), accuracy.item()
+
+    @property
+    def train_dataloader(self):
+        return self.dataset.train_dataloader
+
+    @property
+    def test_dataloader(self):
+        return self.dataset.test_dataloader
+
+    @property
+    def optimizer(self):
+        return torch.optim.Adam([{"params": self.encoder.parameters()}, {"params": self.decoder.parameters()}], lr=self.hyper["learning_rate"])
+
+    def save(self, filepath: path) -> None:
+        filepath = path(filepath)
+        filepath.parent.mkdir()
+        saved = dict()
+        saved["hyper"] = self.hyper
+        saved["encoder state"] = self.encoder.state_dict()
+        saved["decoder state"] = self.decoder.state_dict()
+        saved["embedding"] = self.encoder.embedding
+        torch.save(saved, filepath)
+
+    @staticmethod
+    def load(filepath: str, **kwargs) -> "Trainer":
+        saved = torch.load(filepath, map_location=get_device("cuda"))
+        hyper = table(saved["hyper"])
+        hyper["embedding"] = saved["embedding"]
+        hyper.update(kwargs)
+        ret = Trainer(**hyper)
+        ret.encoder.load_state_dict(saved["encoder state"])
+        ret.decoder.load_state_dict(saved["decoder state"])
+        return ret
+
+    def load_state_dict(self, filepath: str, encoder_state=None, decoder_state=None):
+        if isinstance(filepath, Trainer):
+            encoder_loaded = filepath.encoder.state_dict()
+            decoder_loaded = filepath.decoder.state_dict()
+        else:
+            saved = torch.load(filepath, map_location=self.device)
+            encoder_loaded = saved["encoder state"]
+            decoder_loaded = saved["decoder state"]
+        if encoder_state is not None:
+            encoder_loaded = {name: param for name, param in encoder_loaded.items() if name in encoder_state}
+            decoder_loaded = {name: param for name, param in decoder_loaded.items() if name in decoder_loaded}
+
+        self.encoder.load_state_dict(encoder_loaded)
+        self.decoder.load_state_dict(decoder_loaded)
+
+    def train(self, mode=True):
+        self.encoder.train(mode)
+        self.decoder.train(mode)
+
+    def eval(self):
+        self.encoder.eval()
+        self.decoder.eval()
+
+    def inspect(self):
+        return self.encoder.inspect() + self.decoder.inspect()
+
+    def parameters(self):
+        return vector(self.encoder.parameters()) + vector(self.decoder.parameters())
+
+    def named_parameters(self) -> List[Tuple[str, nn.Parameter]]:
+        return vector(self.encoder.named_parameters()).map(lambda name, x: ("encoder." + name, x)) + vector(self.decoder.named_parameters()).map(lambda name, x: ("decoder." + name, x))
